9209b58cbf4d90b36396396010831220f15e4bac
fyl1 <38299430+fyl1@users.noreply.github.com>
2023-04-14 15:31:52 +0300

    delete comment

--- js/script.js
+++ js/script.js
@@ -65,45 +65,37 @@ if (!!textCard) {
   let showMore = document.querySelectorAll(".show-more");
   if (!!textCard && !!showMore) {
     const smoothHeight = (itemSelector, buttonSelector, contentSelector) => {
-      // объявляем основную функцию, которая принимает в качестве параметров селекторы элемента, кнопки внутри элемента и блока с контентом
 
-      const items = document.querySelectorAll(itemSelector); // находим все элементы по переданному селектору в параметре itemSelector и записываем в константу items
+      const items = document.querySelectorAll(itemSelector); 
 
-      if (!items.length) return; // если таких элементов нет, прекращаем выполнение функции
+      if (!items.length) return; 
 
       items.forEach((el) => {
-        // для каждого элемента
-        const button = el.querySelector(buttonSelector); // находим кнопку и записываем в константу button
-        const content = el.querySelector(contentSelector); // находим блок с контентом и записываем в константу content
+        const button = el.querySelector(buttonSelector); 
+        const content = el.querySelector(contentSelector); 
 
         button.addEventListener("click", () => {
-          // при клике на кнопку
           if (el.dataset.open !== "true") {
-            // если значение data-атрибута open у элемента не равно 'true' и блок с контентом еще не отображен
-            el.dataset.open = "true"; // тогда устанавливаем значение 'true'
-            content.style.maxHeight = `${content.scrollHeight}px`; // и блоку с контентом устанавливаем inline-свойсво max-height со вычисленным значением полной высоты этого блока
+            el.dataset.open = "true"; 
+            content.style.maxHeight = `${content.scrollHeight}px`; 
           } else {
-            // если блок с контентом отображен и значение data-атрибута open у элемента равно 'true'
-            el.dataset.open = "false"; // тогда устанавливаем значение 'false'
-            content.style.maxHeight = ""; // и сбрасываем ранее установленное inline-свойсво max-height
+            el.dataset.open = "false";
+            content.style.maxHeight = "";
           }
         });
 
         const onResize = () => {
-          // объявляем функцию onResize, которая будет корректировать значение inline-свойства max-height при изменении размеров окна браузера
           if (el.dataset.open === "true") {
-            // если значение data-атрибута open у элемента равно 'true' (коректировать высоту нужно только если блок контента отображен)
             if (parseInt(content.style.maxHeight) !== content.scrollHeight) {
-              // если текущее значение inline-свойства max-height у блока контента не равно полной высоте
-              content.style.maxHeight = `${content.scrollHeight}px`; // только тогда блоку с контентом корректируем значение inline-свойсва max-height
+              content.style.maxHeight = `${content.scrollHeight}px`; 
             }
           }
         };
 
-        window.addEventListener("resize", onResize); // вызываем функцию onResize при изменении размеров окна браузера
+        window.addEventListener("resize", onResize); 
       });
     };
 
-    smoothHeight(".card-accardeon", ".show-more", ".card-text"); // вызываем основную функцию smoothHeight и передаем в качестве параметров  необходимые селекторы
+    smoothHeight(".card-accardeon", ".show-more", ".card-text");
   }
 }
